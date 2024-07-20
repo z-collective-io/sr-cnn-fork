ba044c8c6d5d2869475cabaed52400ce22a77bcb
Corey Cerovsek <corey@cerovsek.com>
2024-07-20 01:41:10 +0100

    Fix function arguments in main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 import pandas as pd
 
 
-def detect_anomaly(series, threshold, mag_window, score_window, sensitivity, detect_mode):
+def detect_anomaly(series, threshold, mag_window, score_window, sensitivity, detect_mode, batch_size):
     detector = SpectralResidual(series=series, threshold=threshold, mag_window=mag_window, score_window=score_window,
-                                sensitivity=sensitivity, detect_mode=detect_mode)
+                                sensitivity=sensitivity, detect_mode=detect_mode, batch_size=batch_size)
     print(detector.detect())
 
 
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     sample_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "samples"))
     for sample_file in os.listdir(sample_dir):
         sample = pd.read_csv(os.path.join(sample_dir, sample_file))
-        detect_anomaly(sample, THRESHOLD, MAG_WINDOW, SCORE_WINDOW, 99, DetectMode.anomaly_only)
+        detect_anomaly(sample, THRESHOLD, MAG_WINDOW, SCORE_WINDOW, 99, DetectMode.anomaly_only, 0)
